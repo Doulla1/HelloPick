3e55c45b887605b9b3478e90971ea07acbfae913
Abdoulaye <dialloabdoul99c@gmail.com>
2024-09-18 18:38:03 +0200

    fix: Remove redundant code in Administrateur model

--- app/Models/Administrateur.php
+++ app/Models/Administrateur.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
+
 class Administrateur extends User
 {
     /**
-     * Define the default attributes values.
+     * Booted : add a global scope to limit the model to administrators only
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope('administrateur', function (Builder $builder) {
+            $builder->where('type', 'administrateur');
+        });
+    }
+
+    /**
+     * Define the default attributes values when creating a new model
      *
      */
     protected $attributes = [
