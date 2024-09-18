32bcf7a00d06caf59df5b743fe3466ae5cf925c1
Abdoulaye <dialloabdoul99c@gmail.com>
2024-09-18 19:01:42 +0200

    feat: Install api and add EnsureAdmin Middleware

---
+++ app/Http/Middleware/EnsureIsAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the user is authenticated
+        if (!Auth::check()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // Check if the user is an administrator
+        if (!Auth::user()->isAdmin()) {
+            return response()->json(['message' => 'Forbidden - Admins only'], 403);
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureIsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,11 +8,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // Adding the api route file
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api (append: [
+            EnsureIsAdmin::class, // Checks if the user is an admin
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
