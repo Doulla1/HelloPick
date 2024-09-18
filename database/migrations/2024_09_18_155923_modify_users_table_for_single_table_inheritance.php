0dbf2d46b25aca1bbd0e9224f26d3f111c30eb2f
Abdoulaye <dialloabdoul99c@gmail.com>
2024-09-18 18:02:16 +0200

    db: New migration to table users, remove name and add type

---
+++ database/migrations/2024_09_18_155923_modify_users_table_for_single_table_inheritance.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name'); // Suppression de la colonne name
+            $table->string('type')->default('administrateur'); // Colonne type pour différencier les utilisateurs
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->nullable();
+            $table->dropColumn('type');
+        });
+    }
+};
